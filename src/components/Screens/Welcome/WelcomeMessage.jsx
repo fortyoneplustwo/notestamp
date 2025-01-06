52dc5560681727c5645e8971b68ff323e61f88d9
olive <oambrois@uwaterloo.ca>
2025-01-06 03:28:07 -0500

    Refactors styling using tailwind and rewrites welcome message

--- src/components/Screens/Welcome/WelcomeMessage.jsx
+++ src/components/Screens/Welcome/WelcomeMessage.jsx
@@ -1,46 +1,38 @@
 import React from 'react'
 
-const WelcomeMessage = () => {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-      <div style={{ textJustify: 'inter-word', width: '80%', overflow: 'auto', height: '80%' }}>
-        <p style={{ whiteSpace: 'pre-wrap' }}>
-          Welcome to <span style={{ fontWeight: 'bold', color: 'orangered' }}>notestamp</span>, a web app that synchronizes your notes to media.
-        </p>
-        <br></br>
-        <p style={{ whiteSpace: 'pre-wrap' }}>Instructions:</p>
-        <p>
-          <li>When recording or viewing media, press <code>&lt;enter&gt;</code> to insert a stamp.</li>
-          <li>Press <code>&lt;shift + enter&gt;</code> to escape stamping.</li>
-          <li>Click a stamp and instantly seek the media to the stamp value.</li>
-          <li>Your notes persist across page reloads unless you clear the browser cache.</li>
-        </p>
-        <br />
-        <p style={{ whiteSpace: 'pre-wrap' }}>Managing projects in Sync Mode:</p>
-        <p>
-          <li>Toggle the <code>Sync</code> button to enable synchronization.</li>
-          <li>Click <code>Open directory</code> to pick a folder on your device.</li>
-          <li>In Sync Mode, you are able to save a project to the chosen directory.</li>
-          <li>Any projects saved to that directory will be listed in the left pane.</li>
-        </p>
-        <br></br>
-        <p style={{ whiteSpace: 'pre-wrap' }}>
-          <strong style={{ color: 'red' }}>
-            Warning: </strong>This app is still in development and has only been tested on Chrome desktop.
-        </p>
-        <br></br>
-        <p style={{ whiteSpace: 'pre-wrap' }}>
-          Features in development:
-          <li>Dark Mode</li>
-          <li>Collaborative editing</li>
-        </p>
-        <br></br>
-        <p>This project is open source. Contribute on&nbsp; 
-          <a href='https://github.com/fortyoneplustwo/notestamp' className='text-blue-600'>github</a>.
-        </p>
-      </div>
+const WelcomeMessage = () => (
+  <>
+    <div className="h-full w-full overflow-auto p-[10%]">
+      <p>
+        Welcome to <span className="text-[orangered] font-bold">notestamp</span>, a web app that synchronizes your notes to media.
+      </p>
+      <br></br>
+      <p>Instructions:</p>
+      <p>
+        <li>When recording or viewing media, press <code>&lt;enter&gt;</code> to insert a stamp.</li>
+        <li>Press <code>&lt;shift + enter&gt;</code> to escape stamping.</li>
+        <li>Click a stamp and instantly seek the media to the stamp value.</li>
+        <li>Your notes persist across page reloads unless you clear the browser cache.</li>
+      </p>
+      <br />
+      <p>Since the backend is still under development, we have released a feature that lets you save your projects locally. Toggle the <code>File Sync</code> switch to get started.</p>
+      <br></br>
+      <p>
+        <strong className="text-red-500">
+          Warning: </strong>This app works best on Chrome desktop.
+      </p>
+      <br></br>
+      <p>
+        Features to come:
+        <li>Cloud storage</li>
+        <li>Collaborative editing</li>
+      </p>
+      <br></br>
+      <p>This project is open source. Report issues on&nbsp; 
+        <a href='https://github.com/fortyoneplustwo/notestamp' className='text-blue-600'>github</a>.
+      </p>
     </div>
-  )
-}
+  </>
+)
 
 export default WelcomeMessage
