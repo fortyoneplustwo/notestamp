9de13f2c4ceda8bf6a5f106d04c66d0e0e26d082
Olivier Ambroise <oambrois@uwaterloo.ca>
2026-01-20 16:32:53 +0400

    build: add rollup visualizer

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,23 @@
 import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: "build",
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      visualizer({
+        template: "treemap", // or 'sunburst'
+        open: true, // opens the browser automatically after build
+        gzipSize: true, // shows you the compressed size
+        brotliSize: true, // shows brotli size
+        filename: "analyse.html", // name of the output file
+      }),
+    ],
     resolve: {
       alias: {
         "~": path.resolve(__dirname, "."), // eslint-disable-line
